a885a755949de624ca6e047d057393bd32d531c0
stevegibby <34577645+stevegibby@users.noreply.github.com>
2020-04-15 10:22:15 +0100

    TEST7

---
+++ ddlscripts/TEST_7.sql
@@ -0,0 +1,10 @@
+-- Jira EUBO-47 Data Fix
+-- Steve Gibby 22/11/2018
+UPDATE MRAEU.CORE_MIGRATION_DATA c
+   SET processed_flag = 'N',
+       log_desc = NULL,
+       gender = NULL
+ WHERE gender is NOT NULL
+   AND gender = 'U';
+   
+COMMIT;
